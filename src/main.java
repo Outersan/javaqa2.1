b0269a2bcd1b0da847c3ad029ffd866d0d33098c
Kot E <ekattow9@gmail.com>
2022-08-01 13:04:01 +0300

    first

---
+++ src/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 6564;
+        int bonus = ticketPrice / 20;
+
+        System.out.println(bonus);
+    }
+}
